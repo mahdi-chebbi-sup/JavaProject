77f1737627823ba56a93d6029f13f0224ae72763
mahdi-chebbi-sup <83556945+mahdi-chebbi-sup@users.noreply.github.com>
2023-07-21 17:21:26 +0100

    Update Main.java
    testtt

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main{
 	public static void main(String[] args)
-                                      {cc
+                                      {
                                         System.out.println("Hello, Samou7a");
                                       }
                   }
